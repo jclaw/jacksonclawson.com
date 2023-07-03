68a34aa595a769225fbb27edadbee8a80b78d468
Jackson Clawson <jclaw3456@gmail.com>
2023-07-02 18:50:34 -0600

    add wip page, get bundles set up

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,8 @@
+const pluginBundle = require("@11ty/eleventy-plugin-bundle");
+
 module.exports = function(eleventyConfig) {
+    eleventyConfig.addPlugin(pluginBundle);
+
     // Copy the `img` and `css` folders to the output
     eleventyConfig.addPassthroughCopy("img");
-    eleventyConfig.addPassthroughCopy("css");
 };
---
+++ js/canvas.js
@@ -0,0 +1,12 @@
+(function () {
+  
+  function draw() {
+    const canvas = document.getElementById("tutorial");
+    if (canvas.getContext) {
+      const ctx = canvas.getContext("2d");
+    }
+  }
+  draw();
+
+})();
+
