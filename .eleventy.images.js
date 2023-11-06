76430a7580aeb02be098fafb60e087ba7b1ee4dd
Jackson Clawson <jclaw3456@gmail.com>
2023-11-05 23:40:17 -0500

    add bandcamp daily article and images, ability to use gifs and add classes to images, basic styles

--- .eleventy.images.js
+++ .eleventy.images.js
@@ -1,5 +1,8 @@
 const path = require('path')
 const Image = require('@11ty/eleventy-img')
+const fs = require('fs');
+const unsupportedFormats = ['.gif'];
+const outputDir = './_site/img/'
 
 module.exports = function (eleventyConfig) {
   function relativeToInputPath (inputPath, relativeFilePath) {
@@ -8,24 +11,46 @@ module.exports = function (eleventyConfig) {
 
     return path.resolve(split.join(path.sep), relativeFilePath)
   }
+  function relativeToImgPath (relativeFilePath) {
+    const split = relativeFilePath.split('/')
 
-  eleventyConfig.addShortcode('image', async function imageShortcode (src, alt, sizes) {
+    return `/img/${split[1]}`
+  }
+
+  eleventyConfig.addShortcode('image', async function imageShortcode (src, alt, sizes, klass) {
     // Full list of formats here: https://www.11ty.dev/docs/plugins/image/#output-formats
     const file = relativeToInputPath(this.page.inputPath, src)
-    const metadata = await Image(file, {
-      widths: [300, 600, 900, 1200],
-      formats: ['avif', 'jpeg'],
-      outputDir: './_site/img/'
-    })
+    const { ext } = path.parse(src);
 
-    const imageAttributes = {
-      alt,
-      sizes,
-      loading: 'lazy',
-      decoding: 'async'
+    if (unsupportedFormats.includes(ext)) {
+      if(alt === undefined) {
+        throw new Error(`Missing \`alt\` attribute on eleventy-img shortcode from: ${src}`);
+      }
+      // TODO
+      // use fs.copyFile to manually copy this file to your output dir
+      // based on this: https://github.com/11ty/eleventy-img/blob/46fe08739c70326bab34bbb5208b6c91891d38be/generate-html.js#L19
+      const newFile = src.split('/')[1]
+      fs.copyFile(file, `${outputDir}${newFile}`, (err) => {
+        throw new Error(err)
+      })
+      return `<img src="/img/${newFile}" alt="${alt}" />`;
+    } else {
+      const metadata = await Image(file, {
+        widths: [300, 600, 900, 1200],
+        formats: ['avif', 'jpeg'],
+        outputDir: outputDir
+      })
+
+      const imageAttributes = {
+        class: klass,
+        alt,
+        sizes,
+        loading: 'lazy',
+        decoding: 'async'
+      }
+
+      // You bet we throw an error on a missing alt (alt="" works okay)
+      return Image.generateHTML(metadata, imageAttributes)
     }
-
-    // You bet we throw an error on a missing alt (alt="" works okay)
-    return Image.generateHTML(metadata, imageAttributes)
   })
 }
--- projects/bandcamp-daily/bandcamp-daily.md
+++ projects/bandcamp-daily/bandcamp-daily.md
@@ -3,4 +3,29 @@ title: "Bandcamp Daily"
 priority: 1
 ---
 
-hi
+<section>
+    <div class="text-column">
+        <p><a href="https://daily.bandcamp.com">view site</a></p>
+        <p>
+            The Bandcamp Daily is the editorial publication arm of Bandcamp that spotlights the best, most interesting, and strangest music on the platform from across the globe. It’s a treasure trove of wonderful writing and careful curation. The humans that run it are brave and gifted explorers of the Bandcamp-verse. Yes, that’s right — music curation by real humans :)
+        </p>
+        <p>
+            I was the lead engineer on the team for the redesign effort. I built the site using a third-party headless CMS to power a frontend built within the Bandcamp ecosystem. The redesign accomplished the following things:
+            <ul>
+                <li>Ability to wishlist</li>
+                <li>Ability to follow artist</li>
+                <li>Improved player experience, displaying artist’s merch</li>
+                <li>Improved reading experience: listen while reading</li>
+                <li>Customized look and feel, made it feel like Bandcamp, moved from dated blog template to professional publication</li>
+                <li>Expose many more opportunities for exploring more articles: view-all menu, franchises, pagination, contributors, genres</li>
+            </ul>
+        </p>
+    </div>
+    <div class="image-column">
+        {% image "./bandcamp-navigator-list-square.jpg", "Screenshot of an article on the Bandcamp Daily called 'Bandcamp Navigator, September 2023'", "200vw", "outline" %}
+        {% image "./player.jpg", "", "200vw", "outline" %}
+    </div>
+    <div class="center">
+        {% image "./older-posts.gif", "", "200vw", "outline" %}
+    </div>
+</section>
